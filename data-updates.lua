de04c7e12961b4a552d16d08ca9c9d6340ddbd53
billbo99 <cactus_computers-github@yahoo.com>
2020-11-24 12:05:48 +0000

    1.1

--- data-updates.lua
+++ data-updates.lua
@@ -8,6 +8,9 @@ if not data.raw["assembling-machine"]["centrifuge"].fast_replaceable_group then
     data.raw["assembling-machine"]["centrifuge"].fast_replaceable_group = "centrifuge"
     data.raw["assembling-machine"]["centrifuge-mk2"].fast_replaceable_group = "centrifuge"
     data.raw["assembling-machine"]["centrifuge-mk3"].fast_replaceable_group = "centrifuge"
+else
+    data.raw["assembling-machine"]["centrifuge-mk2"].fast_replaceable_group = data.raw["assembling-machine"]["centrifuge"].fast_replaceable_group
+    data.raw["assembling-machine"]["centrifuge-mk3"].fast_replaceable_group = data.raw["assembling-machine"]["centrifuge"].fast_replaceable_group
 end
 
 if not data.raw["assembling-machine"]["centrifuge"].next_upgrade then
@@ -19,6 +22,9 @@ if not data.raw["assembling-machine"]["chemical-plant"].fast_replaceable_group t
     data.raw["assembling-machine"]["chemical-plant"].fast_replaceable_group = "chemical-plant"
     data.raw["assembling-machine"]["chemical-plant-mk2"].fast_replaceable_group = "chemical-plant"
     data.raw["assembling-machine"]["chemical-plant-mk3"].fast_replaceable_group = "chemical-plant"
+else
+    data.raw["assembling-machine"]["chemical-plant-mk2"].fast_replaceable_group = data.raw["assembling-machine"]["chemical-plant"].fast_replaceable_group
+    data.raw["assembling-machine"]["chemical-plant-mk3"].fast_replaceable_group = data.raw["assembling-machine"]["chemical-plant"].fast_replaceable_group
 end
 
 if not data.raw["assembling-machine"]["chemical-plant"].next_upgrade then
@@ -30,6 +36,9 @@ if not data.raw["furnace"]["electric-furnace"].fast_replaceable_group then
     data.raw["furnace"]["electric-furnace"].fast_replaceable_group = "electric-furnace"
     data.raw["furnace"]["electric-furnace-mk2"].fast_replaceable_group = "electric-furnace"
     data.raw["furnace"]["electric-furnace-mk3"].fast_replaceable_group = "electric-furnace"
+else
+    data.raw["furnace"]["electric-furnace-mk2"].fast_replaceable_group = data.raw["furnace"]["electric-furnace"].fast_replaceable_group
+    data.raw["furnace"]["electric-furnace-mk3"].fast_replaceable_group = data.raw["furnace"]["electric-furnace"].fast_replaceable_group
 end
 
 if not data.raw["furnace"]["electric-furnace"].next_upgrade then
@@ -41,6 +50,9 @@ if not data.raw["mining-drill"]["electric-mining-drill"].fast_replaceable_group 
     data.raw["mining-drill"]["electric-mining-drill"].fast_replaceable_group = "electric-mining-drill"
     data.raw["mining-drill"]["electric-mining-drill-mk2"].fast_replaceable_group = "electric-mining-drill"
     data.raw["mining-drill"]["electric-mining-drill-mk3"].fast_replaceable_group = "electric-mining-drill"
+else
+    data.raw["mining-drill"]["electric-mining-drill-mk2"].fast_replaceable_group = data.raw["mining-drill"]["electric-mining-drill"].fast_replaceable_group
+    data.raw["mining-drill"]["electric-mining-drill-mk3"].fast_replaceable_group = data.raw["mining-drill"]["electric-mining-drill"].fast_replaceable_group
 end
 
 if not data.raw["mining-drill"]["electric-mining-drill"].next_upgrade then
@@ -52,6 +64,9 @@ if not data.raw["lab"]["lab"].fast_replaceable_group then
     data.raw["lab"]["lab"].fast_replaceable_group = "lab"
     data.raw["lab"]["lab-mk2"].fast_replaceable_group = "lab"
     data.raw["lab"]["lab-mk3"].fast_replaceable_group = "lab"
+else
+    data.raw["lab"]["lab-mk2"].fast_replaceable_group = data.raw["lab"]["lab"].fast_replaceable_group
+    data.raw["lab"]["lab-mk3"].fast_replaceable_group = data.raw["lab"]["lab"].fast_replaceable_group
 end
 
 if not data.raw["lab"]["lab"].next_upgrade then
@@ -63,6 +78,9 @@ if not data.raw["assembling-machine"]["oil-refinery"].fast_replaceable_group the
     data.raw["assembling-machine"]["oil-refinery"].fast_replaceable_group = "oil-refinery"
     data.raw["assembling-machine"]["oil-refinery-mk2"].fast_replaceable_group = "oil-refinery"
     data.raw["assembling-machine"]["oil-refinery-mk3"].fast_replaceable_group = "oil-refinery"
+else
+    data.raw["assembling-machine"]["oil-refinery-mk2"].fast_replaceable_group = data.raw["assembling-machine"]["oil-refinery"].fast_replaceable_group
+    data.raw["assembling-machine"]["oil-refinery-mk3"].fast_replaceable_group = data.raw["assembling-machine"]["oil-refinery"].fast_replaceable_group
 end
 
 if not data.raw["assembling-machine"]["oil-refinery"].next_upgrade then
@@ -73,6 +91,8 @@ end
 if not data.raw["mining-drill"]["pumpjack"].fast_replaceable_group then
     data.raw["mining-drill"]["pumpjack"].fast_replaceable_group = "pumpjack"
     data.raw["mining-drill"]["pumpjack-mk2"].fast_replaceable_group = "pumpjack"
+else
+    data.raw["mining-drill"]["pumpjack-mk2"].fast_replaceable_group = data.raw["mining-drill"]["pumpjack"].fast_replaceable_group
 end
 
 if not data.raw["mining-drill"]["pumpjack"].next_upgrade then
@@ -83,6 +103,8 @@ end
 if not data.raw["radar"]["radar"].fast_replaceable_group then
     data.raw["radar"]["radar"].fast_replaceable_group = "radar"
     data.raw["radar"]["radar-mk2"].fast_replaceable_group = "radar"
+else
+    data.raw["radar"]["radar-mk2"].fast_replaceable_group = data.raw["radar"]["radar"].fast_replaceable_group
 end
 
 if not data.raw["radar"]["radar"].next_upgrade then
